263434b225d36efa65176650c84356c713d538e0
Jonah Starling <starling.jonah@gmail.com>
2020-03-20 15:54:47 -0400

    Adds Firebase and Cocoapods

---
+++ WheresTheTP/Pods/Target Support Files/Pods-WheresTheTP/Pods-WheresTheTP-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WheresTheTP : NSObject
+@end
+@implementation PodsDummy_Pods_WheresTheTP
+@end
---
+++ WheresTheTP/Pods/Target Support Files/Pods-WheresTheTPTests/Pods-WheresTheTPTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WheresTheTPTests : NSObject
+@end
+@implementation PodsDummy_Pods_WheresTheTPTests
+@end
